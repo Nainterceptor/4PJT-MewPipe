c1777620f8fb6f84cb932f1b5fcdb715635560ce
Gaël Demette <demette.gael@gmail.com>
2015-04-03 18:31:29 +0200

    useful snippets

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-go get labix.org/v2/mgo
+go get gopkg.in/mgo.v2
+go get gopkg.in/mgo.v2/bson
 go get github.com/emicklei/go-restful
+go get -u -a github.com/vharitonsky/iniflags
