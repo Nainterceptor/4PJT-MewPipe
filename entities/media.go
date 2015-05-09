a35d92749deafd56dd6d282163cf5375459b9b1a
Gaël Demette <demette.gael@gmail.com>
2015-05-09 18:31:05 +0200

    WIP Upload and Read
    refs #398 #395

--- entities/media.go
+++ entities/media.go
@@ -5,13 +5,16 @@ import(
     "gopkg.in/mgo.v2/bson"
 )
 
+type user struct {
+    *User
+}
 
 type Media struct {
     Id          bson.ObjectId   `json:"id" bson:"_id,omitempty"`
     Title       string          `json:"title"`
     Summary     string          `json:"summary"`
-    Publisher   User   `json:"user"`
-    File        bson.ObjectId   `json:"file,omitempty"`
+    Publisher   user            `json:"user,omitempty" bson:"user,omitempty"`
+    File        bson.ObjectId   `json:"file,omitempty" bson:"file,omitempty"`
 }
 
 var MediaCollection = configs.MongoDB.C("media")
--- rest/media.go
+++ rest/media.go
@@ -5,6 +5,9 @@ import (
     "supinfo/mewpipe/entities"
     "net/http"
     "io"
+    "gopkg.in/mgo.v2/bson"
+    "strconv"
+    "regexp"
 )
 
 func MediaRoute() *restful.WebService {
@@ -15,7 +18,8 @@ func MediaRoute() *restful.WebService {
     Produces(restful.MIME_JSON)
 
     service.Route(service.POST("").To(mediaCreate))
-    service.Route(service.POST("/upload").Consumes("multipart/form-data").To(mediaUpload))
+    service.Route(service.POST("/{media-id}/upload").Consumes("multipart/form-data").To(mediaUpload))
+    service.Route(service.GET("/{media-id}/read").To(mediaRead))
 
     return service
 }
@@ -23,31 +27,124 @@ func MediaRoute() *restful.WebService {
 func mediaCreate(request *restful.Request, response *restful.Response) {
 
     media := entities.Media{}
-    errRE := request.ReadEntity(&media)
+    if err := request.ReadEntity(&media); err != nil {
+        response.WriteError(http.StatusInternalServerError, err)
+        return
+    }
+    media.Id = bson.NewObjectId()
 
     if err := entities.MediaCollection.Insert(&media); err != nil {
         response.WriteError(http.StatusInternalServerError, err)
         return
     }
-
-    if errRE == nil {
-        response.WriteEntity(media)
-    } else {
-        response.WriteError(http.StatusInternalServerError, errRE)
-    }
+    response.WriteEntity(media)
 }
 
 func mediaUpload(request *restful.Request, response *restful.Response) {
+    id := request.PathParameter("media-id")
+    if !bson.IsObjectIdHex(id) {
+        response.WriteErrorString(404, "Bad ID")
+        return
+    }
+    oid := bson.ObjectIdHex(id)
+    media := entities.Media{}
+
+    if err := entities.MediaCollection.FindId(oid).One(&media); err != nil {
+        response.WriteError(http.StatusInternalServerError, err)
+        return
+    }
+
     request.Request.ParseMultipartForm(500 * 1000 * 1000)
     postedFile, handler, err := request.Request.FormFile("file")
     if err != nil {
         response.WriteError(http.StatusInternalServerError, err)
+        return
     }
     defer postedFile.Close()
     mongoFile, err := entities.MediaGridFS.Create(handler.Filename)
     defer mongoFile.Close()
-    io.Copy(mongoFile, postedFile)
     if err != nil {
         response.WriteError(http.StatusInternalServerError, err)
+        return
     }
+    io.Copy(mongoFile, postedFile)
+    mongoFile.SetContentType(handler.Header.Get("Content-Type"))
+    media.File = mongoFile.Id().(bson.ObjectId)
+    if err := entities.MediaCollection.UpdateId(oid,&media); err != nil {
+        response.WriteError(http.StatusInternalServerError, err)
+        mongoFile.Abort()
+        return
+    }
+    response.WriteEntity(media)
+}
+
+func mediaRead(request *restful.Request, response *restful.Response) {
+    id := request.PathParameter("media-id")
+    if !bson.IsObjectIdHex(id) {
+        response.WriteErrorString(404, "Bad ID")
+        return
+    }
+    oid := bson.ObjectIdHex(id)
+    media := entities.Media{}
+
+    if err := entities.MediaCollection.FindId(oid).One(&media); err != nil {
+        response.WriteError(http.StatusInternalServerError, err)
+        return
+    }
+
+    mongoFile, err := entities.MediaGridFS.OpenId(media.File)
+    defer mongoFile.Close()
+    if err != nil {
+        response.WriteError(http.StatusInternalServerError, err)
+        return;
+    }
+    headerWriter := response.ResponseWriter.Header()
+    headerWriter.Add("Accept-Ranges", "bytes")
+    headerWriter.Add("Content-Disposition", "attachment; filename=video.mp4")
+    headerWriter.Add("Content-Length", strconv.FormatInt(mongoFile.Size(), 10))
+    headerWriter.Add("Content-type", mongoFile.ContentType())
+    if rangeReq := request.Request.Header.Get("range"); rangeReq != "" {
+        regex, _ := regexp.Compile(`bytes=([0-9]*)-([0-9]*)`)
+        ranges := regex.FindStringSubmatch(rangeReq)
+        start := 0;
+        intSize := int(mongoFile.Size())
+        end := intSize - 1
+        if len(ranges) > 2 {
+            testedStart, errStart := strconv.Atoi(ranges[1])
+            if errStart == nil {
+                start = testedStart
+            }
+            testedEnd, errEnd := strconv.Atoi(ranges[2])
+            if errEnd == nil {
+                end = testedEnd
+            }
+
+            if errStart == nil && errEnd != nil {
+                start = testedStart
+                end = intSize - 1
+            } else if errStart != nil && errEnd == nil {
+                start = intSize - testedEnd
+                end = intSize - 1
+            }
+            headerWriter.Add("Content-Range", "bytes " + strconv.Itoa(start) + "-" + strconv.Itoa(end) + "/" + strconv.Itoa(intSize))
+            _, err := mongoFile.Seek(int64(start), 0)
+            if err != nil {
+                response.WriteError(http.StatusInternalServerError, err)
+                return;
+            }
+            buffer := make([]byte, end - start)
+            _, err = mongoFile.Read(buffer)
+            if err != nil {
+                response.WriteError(http.StatusInternalServerError, err)
+                return;
+            }
+            response.WriteHeader(http.StatusPartialContent)
+            if _, err := response.Write(buffer); err != nil {
+                response.WriteError(http.StatusInternalServerError, err)
+                return;
+            }
+            return
+        }
+    }
+    _, err = io.Copy(response, mongoFile)
 }
