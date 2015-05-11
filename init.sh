fc5b6ffd4f9db6baacae3aca7348de9cc8127c2e
Gaël Demette <demette.gael@gmail.com>
2015-05-11 15:57:43 +0200

    Useless, thanks Robin

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-go get gopkg.in/mgo.v2
-go get gopkg.in/mgo.v2/bson
-go get github.com/emicklei/go-restful
-go get -u -a github.com/vharitonsky/iniflags
-go get golang.org/x/crypto/bcrypt
-go get github.com/dgrijalva/jwt-go
