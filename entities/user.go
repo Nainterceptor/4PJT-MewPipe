203f01432f86a843dffb331969bf4a13f068f780
Gaël Demette <demette.gael@gmail.com>
2015-05-11 14:23:20 +0200

    Trying a better pattern
    * POST /users

--- entities/user.go
+++ entities/user.go
@@ -3,24 +3,68 @@ package entities
 import(
     "supinfo/mewpipe/configs"
     "gopkg.in/mgo.v2/bson"
+    "golang.org/x/crypto/bcrypt"
+    "errors"
 )
 
 type name struct {
     FirstName   string  `json:"firstname"`
-    LastName   string  `json:"lastname"`
-}
-
-type User struct {
-    Id          bson.ObjectId   `json:"id" bson:"_id,omitempty"`
-    Name        name            `json:"name" bson:",omitempty"`
-    Email       string          `json:"email" bson:",omitempty"`
-    Password    string    `json:"password" bson:",omitempty"`
-    HashedPassword    string    `json:"hashedpassword" bson:",omitempty"`
-    UserTokens  []UserToken     `json:"usertokens" bson:",omitempty"`
+    LastName    string  `json:"lastname"`
+    NickName    string  `json:"nickname"`
 }
 
 type UserToken struct {
     Token   string `json:"token"`
 }
 
-var UserCollection = configs.MongoDB.C("users")
+type User struct {
+    Id              bson.ObjectId   `json:"id" bson:"_id,omitempty"`
+    Name            name            `json:"name" bson:",omitempty"`
+    Email           string          `json:"email" bson:",omitempty"`
+    Roles           []string        `json:"-" bson",omitempty"`
+    Password        string          `json:"password,omitempty" bson:",omitempty"`
+    HashedPassword  string          `json:"-" bson:",omitempty"`
+    UserTokens      []UserToken     `json:"-" bson:",omitempty"`
+}
+
+func UserNew() *User {
+    user := new(User)
+    user.Id = bson.NewObjectId()
+    return user
+}
+
+func (u *User) Validate() error {
+    if u.Email == "" {
+        return errors.New("`email` is empty")
+    }
+    if u.Name.NickName == "" {
+        return errors.New("`nickname` is empty")
+    }
+    return nil
+}
+
+func (u *User) Insert() error {
+    if u.Password == "" {
+        return errors.New("`password` is empty")
+    }
+    u.hashPassword()
+    if err := userCollection.Insert(&u); err != nil {
+        return err
+    }
+    return nil
+}
+
+func (u *User) clean() {
+    u.Password = ""
+}
+
+func (u *User) hashPassword() error {
+    defer u.clean()
+
+    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), 10)
+    u.HashedPassword = string(hashedPassword[:])
+
+    return err
+}
+
+var userCollection = configs.MongoDB.C("users")
--- rest/user.go
+++ rest/user.go
@@ -4,12 +4,6 @@ import (
 	"github.com/emicklei/go-restful"
 	"supinfo/mewpipe/entities"
 	"net/http"
-	"gopkg.in/mgo.v2/bson"
-	"supinfo/mewpipe/utils"
-	"golang.org/x/crypto/bcrypt"
-	"github.com/dgrijalva/jwt-go"
-	"time"
-	"fmt"
 )
 
 func UserRoute() *restful.WebService {
@@ -19,197 +13,28 @@ func UserRoute() *restful.WebService {
 	Consumes(restful.MIME_JSON).
 	Produces(restful.MIME_JSON)
 
-	service.Route(service.POST("/login").To(Connexion)).
-		Doc("Login").
-		Param(service.BodyParameter("password", "the form password").DataType("string"))
-	service.Route(service.POST("").To(CreateUser))
-	service.Route(service.GET("/{user-id}").To(GetUser)).
-		Doc("get a user")
-	service.Route(service.PUT("/update/{user-id}").To(UpdateUser)).
-		Doc("update a single user")
-	service.Route(service.DELETE("/delete/{user-id}").To(DeleteUser)).
-		Doc("delete a single user")
+	service.Route(service.POST("").To(createUser))
 
 	return service
 }
 
-func NewToken(usr entities.User, mySigningKey []byte) (error) {
+func createUser(request *restful.Request, response *restful.Response) {
 
-	token := jwt.New(jwt.GetSigningMethod("HS256"))
-	token.Claims["userid"] = usr.Id
-	token.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
-	tokenString, err := token.SignedString(mySigningKey)
+	usr := entities.UserNew();
 
-	usrToken := entities.UserToken{ tokenString }
-
-	usr.UserTokens = append(usr.UserTokens, usrToken)
-
-	if err := entities.UserCollection.UpdateId(usr.Id, &usr); err != nil {
-		panic(err)
+	if err := request.ReadEntity(&usr); err != nil {
+		response.WriteError(http.StatusBadRequest, err)
 	}
 
-	return err
-}
+    if err := usr.Validate(); err != nil {
+        response.WriteError(http.StatusNotAcceptable, err)
+        return
+    }
 
-func CheckToken(myToken string) (error) {
-
-	token, err := jwt.Parse(myToken, func(token *jwt.Token) (interface{}, error) {
-		return []byte("mypipe"), nil
-	})
-
-	if token.Valid {
-		fmt.Println("Token ok")
-	} else if ve, ok := err.(*jwt.ValidationError); ok {
-		if ve.Errors&jwt.ValidationErrorMalformed != 0 {
-			fmt.Println("Invalid token")
-		} else if ve.Errors&(jwt.ValidationErrorExpired|jwt.ValidationErrorNotValidYet) != 0 {
-			fmt.Println("Token expired", err)
-		} else {
-			fmt.Println("Couldn't handle this token: ", err)
-		}
-	} else {
-		fmt.Println("Couldn't handle this token: ", err)
-	}
-
-	return err
-}
-
-func Connexion(request *restful.Request, response *restful.Response) {
-
-	connexion := entities.User{}
-	usr := entities.User{}
-	errRE := request.ReadEntity(&connexion)
-
-	err := entities.UserCollection.Find(bson.M{"email": connexion.Email}).One(&usr)
-	if err != nil {
+	if err := usr.Insert(); err != nil {
 		response.WriteError(http.StatusInternalServerError, err)
 		return
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(usr.HashedPassword), []byte(connexion.Password))
-	if err != nil {
-		panic(err)
-	}
-
-	if errRE == nil {
-		NewToken(usr, []byte("mypipe"))
-		if err != nil {
-			panic(err)
-		}
-
-		response.WriteEntity(usr)
-	} else {
-		response.WriteError(http.StatusInternalServerError, errRE)
-	}
-}
-
-func CreateUser(request *restful.Request, response *restful.Response) {
-
-	usr := entities.User{}
-	errRE := request.ReadEntity(&usr)
-	usr.Id = bson.NewObjectId()
-	usr.HashedPassword = utils.Hash(usr.Password)
-	usr.Password = ""
-
-	if err := entities.UserCollection.Insert(&usr); err != nil {
-		response.WriteError(http.StatusInternalServerError, err)
-		return
-	}
-
-	if errRE == nil {
-		response.WriteEntity(usr)
-	} else {
-		response.WriteError(http.StatusInternalServerError, errRE)
-	}
-}
-
-func GetUser(request *restful.Request, response *restful.Response) {
-
-	id := request.PathParameter("user-id")
-	myToken := request.QueryParameter("token")
-
-	err := CheckToken(myToken)
-	if err != nil {
-		panic(err)
-	}
-
-	if !bson.IsObjectIdHex(id) {
-		response.WriteErrorString(404, "Problem with the id")
-		return
-	}
-
-	oid := bson.ObjectIdHex(id)
-	usr := entities.User{}
-
-	if err := entities.UserCollection.FindId(oid).One(&usr); err != nil {
-		response.WriteError(http.StatusInternalServerError, err)
-		return
-	}
-
-	response.WriteEntity(usr)
-}
-
-func UpdateUser(request *restful.Request, response *restful.Response) {
-
-	id := request.PathParameter("user-id")
-	myToken := request.QueryParameter("token")
-
-	err := CheckToken(myToken)
-	if err != nil {
-		panic(err)
-	}
-
-	if !bson.IsObjectIdHex(id) {
-		response.WriteErrorString(404, "Problem with the id")
-		return
-	}
-
-	oid := bson.ObjectIdHex(id)
-	usr := entities.User{}
-	errRE := request.ReadEntity(&usr)
-
-	usr.HashedPassword = utils.Hash(usr.Password)
-	usr.Password = ""
-
-	if _, err := entities.UserCollection.UpsertId(oid, &usr); err != nil {
-		response.WriteError(http.StatusInternalServerError, err)
-		return
-	}
-
-	if errRE == nil {
-		response.WriteEntity(usr)
-	} else {
-		response.WriteError(http.StatusInternalServerError, errRE)
-	}
-}
-
-func DeleteUser(request *restful.Request, response *restful.Response) {
-
-	id := request.PathParameter("user-id")
-	myToken := request.QueryParameter("token")
-
-	err := CheckToken(myToken)
-	if err != nil {
-		panic(err)
-	}
-
-	if !bson.IsObjectIdHex(id) {
-		response.WriteErrorString(404, "Problem with the id")
-		return
-	}
-
-	oid := bson.ObjectIdHex(id)
-	usr := entities.User{}
-	errRE := request.ReadEntity(&usr)
-
-	if err := entities.UserCollection.RemoveId(oid); err != nil {
-		response.WriteError(http.StatusInternalServerError, err)
-		return
-	}
-
-	if errRE == nil {
-		response.WriteEntity(usr)
-	} else {
-		response.WriteError(http.StatusInternalServerError, errRE)
-	}
+    response.WriteEntity(usr)
 }
