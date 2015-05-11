5891a6c164610088f4e874b74d5ac32d86aaf3f8
Gaël Demette <demette.gael@gmail.com>
2015-05-12 00:18:08 +0200

    indent

--- rest/media/route-doc.go
+++ rest/media/route-doc.go
@@ -7,16 +7,29 @@ import (
 func MediaRoute(container *restful.Container) {
     service := new(restful.WebService)
     service.
-    Path("/rest/media").
-    Consumes(restful.MIME_JSON).
-    Produces(restful.MIME_JSON)
+        Path("/rest/media").
+        Consumes(restful.MIME_JSON).
+        Produces(restful.MIME_JSON)
 
-    service.Route(service.POST("").To(mediaCreate))
-    service.Route(service.PUT("/{media-id}").To(mediaPut))
-    service.Route(service.GET("/{media-id}").To(mediaGet))
-    service.Route(service.DELETE("/{media-id}").To(mediaDelete))
-    service.Route(service.POST("/{media-id}/upload").Consumes("multipart/form-data").To(mediaUpload))
-    service.Route(service.GET("/{media-id}/read").To(mediaRead))
+    service.Route(service.
+        POST("").
+        To(mediaCreate))
+    service.Route(service.
+        PUT("/{media-id}").
+        To(mediaPut))
+    service.Route(service.
+        GET("/{media-id}").
+        To(mediaGet))
+    service.Route(service.
+        DELETE("/{media-id}").
+        To(mediaDelete))
+    service.Route(service.
+        POST("/{media-id}/upload").
+        Consumes("multipart/form-data").
+        To(mediaUpload))
+    service.Route(service.
+        GET("/{media-id}/read").
+        To(mediaRead))
 
     container.Add(service)
 }
