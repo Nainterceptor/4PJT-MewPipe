fb1f4302d91477cd29454bd1634a6fec14732085
Gaël Demette <demette.gael@gmail.com>
2015-05-12 15:31:12 +0200

    A test for an alternative routing

--- server.go
+++ server.go
@@ -6,19 +6,25 @@ import (
 	"github.com/emicklei/go-restful"
 	"supinfo/mewpipe/rest/users"
 	"supinfo/mewpipe/rest/media"
+    "log"
 )
 
 func main() {
     configs.Parse()
 
+    container := restful.NewContainer()
+    configs.StaticRouter(container)
+
+    go func() {
+        server := &http.Server{Addr: ":8181", Handler: container}
+        server.ListenAndServe()
+    }()
+
     wsContainer := restful.NewContainer()
     users.UserRoute(wsContainer)
     media.MediaRoute(wsContainer)
-    configs.StaticRouter(wsContainer)
 
     configs.ConfigureSwagger(wsContainer)
-
-
     server := &http.Server{Addr: *configs.HttpBinding, Handler: wsContainer}
-    server.ListenAndServe()
+    log.Fatal(server.ListenAndServe())
 }
