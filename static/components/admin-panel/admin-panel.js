16eaac375c17c7734399b43a566b196b51d38e40
Fireaxe <alex.vaast@gmail.com>
2015-05-18 17:45:57 +0200

    WIP Pagination  (C'est moche mais personne touche je vais refaire !)

--- static/components/admin-panel/admin-panel.js
+++ static/components/admin-panel/admin-panel.js
@@ -1,9 +1,65 @@
-(function(){
+(function () {
     "use strict";
     angular.module('mewpipe.adminPanel', [])
-        .controller('PanelController', [AdminPanelController])
+        .controller('AdminPanelController', [AdminPanelController])
+        .filter('startFrom', AdminPanelFilter)
     ;
 
-    function AdminPanelController(){
+    function AdminPanelController() {
+        var me = this;
+        this.admin = {
+            users: [
+                {username: "Robert1", email: "test@", createdAt: "10-11-2015"}, {
+                    username: "Robert2",
+                    email: "test@",
+                    createdAt: "10-11-2015"
+                },
+                {username: "Robert3", email: "test@", createdAt: "10-11-2015"}, {
+                    username: "Robert4",
+                    email: "test@",
+                    createdAt: "10-11-2015"
+                },
+                {username: "Robert5", email: "test@", createdAt: "10-11-2015"}, {
+                    username: "Robert6",
+                    email: "test@",
+                    createdAt: "10-11-2015"
+                },
+                {username: "Robert7", email: "test@", createdAt: "10-11-2015"}, {
+                    username: "Robert8",
+                    email: "test@",
+                    createdAt: "10-11-2015"
+                },
+                {username: "Robert9", email: "test@", createdAt: "10-11-2015"}, {
+                    username: "Robert10",
+                    email: "test@",
+                    createdAt: "10-11-2015"
+                },
+                {username: "Robert11", email: "test@", createdAt: "10-11-2015"}, {
+                    username: "Robert12",
+                    email: "test@",
+                    createdAt: "10-11-2015"
+                },
+            ]
+        };
+
+        this.scope = {
+            currentPage: 0,
+            numPerPage: 5,
+            totalItems: me.admin.users.length
+        };
+
+        me.scope.numberOfPages = function () {
+            return Math.ceil(me.admin.users.length / me.scope.numPerPage);
+        };
+    }
+
+    function AdminPanelFilter() {
+        return function (input, start) {
+            if (!input || !input.length) {
+                return;
+            }
+            start = +start; //parse to int
+            return input.slice(start);
+        }
     }
 }());
