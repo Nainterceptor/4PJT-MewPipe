c6eb2697a901cd9845ffefb5b50f8e9d1bdfc4c3
Gaël Demette <demette.gael@gmail.com>
2015-05-18 12:16:49 +0200

    go fmt

--- server.go
+++ server.go
@@ -3,21 +3,21 @@ package main
 import (
 	"net/http"
 	"supinfo/mewpipe/configs"
-	"github.com/emicklei/go-restful"
-	"supinfo/mewpipe/rest/users"
 	"supinfo/mewpipe/rest/media"
+	"supinfo/mewpipe/rest/users"
+
+	"github.com/emicklei/go-restful"
 )
 
 func main() {
 
-    wsContainer := restful.NewContainer()
-    users.UserRoute(wsContainer)
-    media.MediaRoute(wsContainer)
-    configs.StaticRouter(wsContainer)
+	wsContainer := restful.NewContainer()
+	users.UserRoute(wsContainer)
+	media.MediaRoute(wsContainer)
+	configs.StaticRouter(wsContainer)
 
-    configs.ConfigureSwagger(wsContainer)
+	configs.ConfigureSwagger(wsContainer)
 
-
-    server := &http.Server{Addr: *configs.HttpBinding, Handler: wsContainer}
-    server.ListenAndServe()
+	server := &http.Server{Addr: *configs.HttpBinding, Handler: wsContainer}
+	server.ListenAndServe()
 }
