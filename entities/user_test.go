42d64de315926e3cb689d08df663997eba8d2338
Gaël Demette <demette.gael@gmail.com>
2015-06-11 14:05:57 +0200

    Tests about new and Userlist

--- entities/user_test.go
+++ entities/user_test.go
@@ -7,6 +7,8 @@ import (
 
 	"encoding/base64"
 
+	"time"
+
 	. "github.com/smartystreets/goconvey/convey"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
@@ -27,6 +29,60 @@ func init() {
 
 func Wipe() {
 	configs.MongoDB.DropDatabase()
+}
+
+func TestUserNew(t *testing.T) {
+	usr := UserNew()
+	Convey("Test UserNew has informations", t, func() {
+		So(usr.Id, ShouldNotBeEmpty)
+	})
+}
+
+func TestUserTokenNew(t *testing.T) {
+	token := userTokenNew()
+	Convey("Test userToken has informations", t, func() {
+		So(token.Token, ShouldNotBeEmpty)
+		currentTime := time.Now()
+		So(token.ExpireAt.Second(), ShouldEqual, currentTime.Add(time.Second*tokenExpiration).Second()) //Precision on second
+	})
+}
+
+func TestUserNewFromId(t *testing.T) {
+	id := bson.ObjectIdHex("5578b8c4f711886e75dec3fd")
+	usr := UserNewFromId(id)
+	Convey("Test UserNew has right ID", t, func() {
+		So(usr.Id, ShouldEqual, bson.ObjectIdHex("5578b8c4f711886e75dec3fd"))
+	})
+}
+
+func TestUserList(t *testing.T) {
+	Wipe()
+	Convey("Test list users", t, func() {
+		getFooUser().Insert()
+		getBarUser().Insert()
+		getAdminUser().Insert()
+		users, err := UserList(bson.M{}, 0, 10)
+		Convey("UserList should not have an error", func() {
+			So(err, ShouldBeNil)
+		})
+		Convey("UserList should have 3 results", func() {
+			So(len(users), ShouldEqual, 3)
+		})
+		users2, err := UserList(bson.M{}, 0, 2)
+		Convey("UserList 2 should not have an error", func() {
+			So(err, ShouldBeNil)
+		})
+		Convey("UserList should have 2 results", func() {
+			So(len(users2), ShouldEqual, 2)
+		})
+		users3, err := UserList(bson.M{}, 1, 1)
+		Convey("UserList 3 should not have an error", func() {
+			So(err, ShouldBeNil)
+		})
+		Convey("UserList should be same", func() {
+			So(users3[:1][0].Id, ShouldEqual, users[1:2][0].Id)
+		})
+	})
 }
 
 func TestUserValidation(t *testing.T) {
@@ -164,5 +220,26 @@ func getFooUser() *User {
 	usr.Name.FirstName = "Foo"
 	usr.Name.LastName = "Foo"
 	usr.Name.NickName = "Foo"
+	return usr
+}
+
+func getBarUser() *User {
+	usr := UserNewFromId(bson.ObjectIdHex("557975fb2ca00357367f7a98"))
+	usr.Email = "bar@foo.tld"
+	usr.Password = "Bar"
+	usr.Name.FirstName = "Bar"
+	usr.Name.LastName = "Bar"
+	usr.Name.NickName = "Bar"
+	return usr
+}
+
+func getAdminUser() *User {
+	usr := UserNewFromId(bson.ObjectIdHex("557976072ca00357367f7a99"))
+	usr.Email = "Admin@admin.tld"
+	usr.Password = "Admin"
+	usr.Name.FirstName = "Admin"
+	usr.Name.LastName = "Admin"
+	usr.Name.NickName = "Admin"
+	usr.Roles = append(usr.Roles, "Admin")
 	return usr
 }
