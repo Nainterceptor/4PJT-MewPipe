c9b00c253a10c928bed0552638a5c161b017e904
Gaël Demette <demette.gael@gmail.com>
2015-06-11 21:55:44 +0200

    refactored vars for entities

---
+++ entities/entities_test.go
@@ -0,0 +1,26 @@
+package entities
+
+import (
+	"supinfo/mewpipe/configs"
+
+	"io/ioutil"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/dbtest"
+)
+
+var Server dbtest.DBServer
+var Session *mgo.Session
+
+func init() {
+
+	tempDir, _ := ioutil.TempDir("", "testing")
+	Server.SetPath(tempDir)
+
+	Session = Server.Session()
+	configs.MongoDB = Session.DB("test_mewpipe")
+}
+
+func Wipe() {
+	configs.MongoDB.DropDatabase()
+}
