78d064615d0e57c28bcad35c1c7e5b7ca5cb4da8
Gaël Demette <demette.gael@gmail.com>
2015-06-12 02:44:28 +0200

    * View dummy test

---
+++ entities/view_test.go
@@ -0,0 +1,41 @@
+package entities
+
+import (
+	"io/ioutil"
+	"supinfo/mewpipe/configs"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"testing"
+)
+
+func init() {
+
+	tempDir, _ := ioutil.TempDir("", "testing")
+	Server.SetPath(tempDir)
+
+	Session = Server.Session()
+	configs.MongoDB = Session.DB("test_mewpipe")
+}
+
+func TestViewNewAnonymous(t *testing.T) {
+	Convey("Test new Anonymous view", t, func() {
+		Wipe()
+		media := getAmazingMedia()
+		media.Insert()
+		ViewNewAnonymous(media.Id)
+		//Can't be writing unless get Views from a media
+	})
+}
+
+func TestViewNew(t *testing.T) {
+	Convey("Test new Anonymous view", t, func() {
+		Wipe()
+		media := getAmazingMedia()
+		user := getFooUser()
+		user.Insert()
+		media.Insert()
+		ViewNew(user.Id, media.Id)
+		//Can't be writing unless get Views from a media
+	})
+}
