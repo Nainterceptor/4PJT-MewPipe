db50f6a6091b6be1261c023d7b3f7b83c8c4bba2
Gaël Demette <demette.gael@gmail.com>
2015-06-14 12:00:23 +0200

    Cover on share

---
+++ entities/share_test.go
@@ -0,0 +1,40 @@
+package entities
+
+import (
+	"io/ioutil"
+	"supinfo/mewpipe/configs"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"testing"
+)
+
+func init() {
+
+	tempDir, _ := ioutil.TempDir("", "testing")
+	Server.SetPath(tempDir)
+
+	Session = Server.Session()
+	configs.MongoDB = Session.DB("test_mewpipe")
+}
+
+func TestShareCountNewAnonymous(t *testing.T) {
+	Convey("Test new Anonymous share", t, func() {
+		Wipe()
+		media := getAmazingMedia()
+		media.Insert()
+		ShareCountNewAnonymous(media.Id)
+		//Can't be writing unless get Views from a media
+	})
+}
+
+func TestShareCountNew(t *testing.T) {
+	Convey("Test new share", t, func() {
+		Wipe()
+		media := getAmazingMedia()
+		user := getFooUser()
+		user.Insert()
+		media.Insert()
+		ShareCountNew(user.Id, media.Id)
+	})
+}
