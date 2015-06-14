9381b7af3dfcb79d54dc13955906bdfcd584d6d7
Gaël Demette <demette.gael@gmail.com>
2015-06-14 19:49:01 +0200

    * Avoid download * Thumbnail

--- fixtures/entities/media.go
+++ fixtures/entities/media.go
@@ -11,9 +11,61 @@ import (
 
 func ClearMedia() {
 	configs.MongoDB.C("media.chunks").DropCollection()
+	configs.MongoDB.C("media.thumbnails.chunks").DropCollection()
 	configs.MongoDB.C("media.files").DropCollection()
+	configs.MongoDB.C("media.thumbnails.files").DropCollection()
 	configs.MongoDB.C("media").DropCollection()
 	fmt.Println("All media deleted")
+}
+
+func InsertSomeMedia() {
+	var mediaArray []*entities.Media
+	mediaArray = append(mediaArray, getBadMedia())
+	mediaArray = append(mediaArray, getAmazingMedia())
+	for _, media := range mediaArray {
+		if err := media.Insert(); err != nil {
+			panic(err)
+			return
+		}
+		addVideoBin(media)
+
+	}
+	addVideoBin(mediaArray[1])
+	fmt.Println("Media added")
+}
+
+func addVideoBin(media *entities.Media) {
+	//Video
+	in, err := os.Open("fixtures/files/sample.mp4")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fileHeader := new(multipart.FileHeader)
+	fileHeader.Filename = "sample.mp4"
+	fileHeader.Header = textproto.MIMEHeader{
+		"Content-Type": {"video/mp4; charset=UTF-8"},
+	}
+	if err = media.Upload(in, fileHeader); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//Thumbnail
+	thumb, err := os.Open("fixtures/files/sample.jpg")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	thumbHeader := new(multipart.FileHeader)
+	thumbHeader.Filename = "sample.jpg"
+	thumbHeader.Header = textproto.MIMEHeader{
+		"Content-Type": {"video/mp4; charset=UTF-8"},
+	}
+	if err = media.UploadThumbnail(thumb, thumbHeader); err != nil {
+		fmt.Println(err)
+		return
+	}
 }
 
 func getAmazingMedia() *entities.Media {
@@ -40,42 +92,4 @@ func getBadMedia() *entities.Media {
 	media.Scope = entities.Private
 
 	return media
-}
-
-func InsertSomeMedia() {
-	var mediaArray []*entities.Media
-	mediaArray = append(mediaArray, getBadMedia())
-	mediaArray = append(mediaArray, getAmazingMedia())
-	for _, media := range mediaArray {
-		if err := media.Insert(); err != nil {
-			panic(err)
-			return
-		}
-	}
-	addVideo(mediaArray[0], "https://raw.githubusercontent.com/youtube/api-samples/master/java/src/main/resources/sample-video.mp4")
-	addVideo(mediaArray[1], "https://webglsamples.googlecode.com/hg-history/c4129c21f4d99e4d2138512c5a74554bdabb2257/color-adjust/sample-video.mp4")
-	fmt.Println("Media added")
-}
-
-func addVideo(media *entities.Media, link string) {
-	path, err := download(link)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	in, err := os.Open(path)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	header := textproto.MIMEHeader{
-		"Content-Type": {"video/mp4; charset=UTF-8"},
-	}
-	fileHeader := new(multipart.FileHeader)
-	fileHeader.Filename = "sample.mp4"
-	fileHeader.Header = header
-	if err = media.Upload(in, fileHeader); err != nil {
-		fmt.Println(err)
-		return
-	}
 }
